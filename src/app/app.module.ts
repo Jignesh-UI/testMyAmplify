41a51931d5cd7dae6da6d9b0026867d8e170f296
Jignesh Patel <hellojiger@gmail.com>
2023-12-04 00:03:07 +0530

    addedAuthentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+import applifyconfig from './../amplifyconfiguration.json';
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure(applifyconfig);
+
 
 @NgModule({
   declarations: [
@@ -10,7 +16,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AmplifyAuthenticatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
